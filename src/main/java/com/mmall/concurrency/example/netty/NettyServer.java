d81c7a953f6a083f79ab35685a779e7163cea74d
张翔宇 <z83@foxmail.com>
2019-03-17 15:37:29 +0800

    netty相关操作

---
+++ src/main/java/com/mmall/concurrency/example/netty/NettyServer.java
@@ -0,0 +1,40 @@
+package com.mmall.concurrency.example.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NettyServer {
+
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        //负责创建新链接
+        NioEventLoopGroup boos = new NioEventLoopGroup();
+        //负责读取数据和业务逻辑处理
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap
+                .group(boos, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                log.info(msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8000);
+    }
+
+}
