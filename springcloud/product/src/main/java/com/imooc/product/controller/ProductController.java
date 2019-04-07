5f7feed5a5aa757721765e1618602488e9f17ff0
teerus <z83@foxmail.com>
2019-04-07 14:35:48 +0800

    添加数据库依赖

---
+++ springcloud/product/src/main/java/com/imooc/product/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.imooc.product.controller;
+
+/**
+ * @ProjectName: product
+ * @Package: com.imooc.product.controller
+ * @ClassName: ProductController
+ * @Description: java类作用描述
+ * @Author: teerus
+ * @CreateDate: 2019-04-07 14:03
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class ProductController {
+}
