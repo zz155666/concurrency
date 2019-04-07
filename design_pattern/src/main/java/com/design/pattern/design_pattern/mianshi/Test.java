7b1fd23bf6bc003fc7410dfb6e16e1f54a3f6429
张翔宇 <z83@foxmail.com>
2019-04-07 22:22:23 +0800

    修改gradle

---
+++ design_pattern/src/main/java/com/design/pattern/design_pattern/mianshi/Test.java
@@ -0,0 +1,9 @@
+package com.design.pattern.design_pattern.mianshi;
+
+public class Test {
+    public static void main(String[] args) {
+        TestClass testClass;
+        testClass = new TestClass();
+        testClass.inc();
+    }
+}
---
+++ design_pattern/src/main/java/com/design/pattern/design_pattern/mianshi/TestClass.java
@@ -0,0 +1,8 @@
+package com.design.pattern.design_pattern.mianshi;
+
+public class TestClass {
+    private int m;
+    public int inc(){
+        return m+1;
+    }
+}
