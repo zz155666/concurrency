e439fb0183c8debd1c6343e681401883f49409b3
teerus <z83@foxmail.com>
2019-06-09 22:21:51 +0800

    ces

---
+++ myPractice/src/main/java/com/mypractice/concurrencybasic/RandomTest.java
@@ -0,0 +1,28 @@
+package com.mypractice.concurrencybasic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.LongAdder;
+
+@Slf4j
+public class RandomTest {
+
+    public static void main(String[] args) {
+//        Random random=new Random();
+//
+//        for(int i=0;i<10;i++){
+//            log.info(random.nextInt(5)+"");
+//        }
+
+        ThreadLocalRandom random=ThreadLocalRandom.current();
+
+        for(int i=0;i<10;++i){
+            log.info(random.nextInt(5)+"");
+        }
+      //  LongAdder
+
+    }
+
+}
