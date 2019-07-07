3d7d8eef47debeead0e0e93b86eeb16e1a06980b
teerus <z83@foxmail.com>
2019-07-07 15:33:52 +0800

    1

---
+++ myPractice/src/main/java/com/mypractice/concurrencybasic/LockSupportTest.java
@@ -0,0 +1,23 @@
+package com.mypractice.concurrencybasic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.LockSupport;
+
+@Slf4j
+public class LockSupportTest {
+    public static void main(String[] args) throws Exception {
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log.info("child thread begin park!");
+                LockSupport.park();
+                log.info("child thread unpark!");
+            }
+        });
+        thread.start();
+        Thread.sleep(1000);
+        log.info("main thread begin unpark!");
+        LockSupport.unpark(thread);
+    }
+}
--- myPractice/src/main/java/com/mypractice/concurrencybasic/RandomTest.java
+++ myPractice/src/main/java/com/mypractice/concurrencybasic/RandomTest.java
@@ -3,6 +3,7 @@ package com.mypractice.concurrencybasic;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -17,6 +18,8 @@ public class RandomTest {
 //        }
 
         ThreadLocalRandom random=ThreadLocalRandom.current();
+
+     //   CopyOnWriteArrayList
 
         for(int i=0;i<10;++i){
             log.info(random.nextInt(5)+"");
